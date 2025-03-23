73a2b001777d238abee526a2be6cb6647f02f2e2
MaxKostov <max.kostov04@gmail.com>
2025-03-24 00:43:02 +0200

    Implemented support for ECHO command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args){
@@ -23,22 +23,61 @@ public class Main {
     }
   }
 
-  public static void clientSocketHandler(Socket clientSocket){
-    try {
-      while (clientSocket.isConnected()) {
-        OutputStream outputStream = clientSocket.getOutputStream();
-        outputStream.write("+PONG\r\n".getBytes());
+  public static void clientSocketHandler(Socket clientSocket) {
+    try (BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8))) {
+
+      while (!clientSocket.isClosed()) {
+        String firstLine = input.readLine();
+        if (firstLine == null) break;
+
+        if (firstLine.startsWith("*")) {
+          int numberOfCommands = Integer.parseInt(firstLine.substring(1));
+          if (numberOfCommands < 1) continue;
+
+          input.readLine();
+          String command = input.readLine();
+
+          if (command.equalsIgnoreCase("ping")) {
+            output.write("+PONG\r\n");
+            output.flush();
+          } else if (command.equalsIgnoreCase("echo")) {
+            if (numberOfCommands > 1) {
+              StringBuilder builder = new StringBuilder();
+              int totalLength = 0;
+
+              for (int i = 1; i < numberOfCommands; i++) {
+                input.readLine();
+                String arg = input.readLine();
+                if (i > 1) {
+                  builder.append(" ");
+                  totalLength++;
+                }
+                builder.append(arg);
+                totalLength += arg.length();
+              }
+
+              output.write("$" + totalLength + "\r\n" + builder + "\r\n");
+              output.flush();
+            } else {
+              output.write("-ERR wrong number of arguments for 'echo' command\r\n");
+              output.flush();
+            }
+          } else {
+            output.write("-ERR unknown command '" + command + "'\r\n");
+            output.flush();
+          }
+        }
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
       try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
+        clientSocket.close();
       } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
+        throw new RuntimeException(e);
       }
     }
   }
+
 }
